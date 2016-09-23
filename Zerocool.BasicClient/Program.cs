e5f0d6a655487499490a59c4defb08bfab78e3ca
Darvell Long <contact@darvell.me>
2016-09-23 02:50:11 -0700

    empty client

---
+++ Zerocool.BasicClient/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Steamworks;
+using Zerocool.API;
+using Zerocool.API.Entities.v1;
+
+namespace Zerocool.BasicClient
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            SteamAPI.Init();
+            HackmudApiClient apiClient = new HackmudApiClient();
+            AuthenticationResult auth = apiClient.AuthenticateAsync(SteamAPIExtension.GetToken()).Result;
+            Console.ReadLine();
+        }
+    }
+}
